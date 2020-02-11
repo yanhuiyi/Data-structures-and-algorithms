f0cd68cf225811e4c738b6d7cacbe10f5f9c3904
Huiyi Yan <yanhuiyi@gmail.com>
2020-02-11 22:30:59 +0800

    Correct LIS

--- src/com/leetcode/problems/Problems300.java
+++ src/com/leetcode/problems/Problems300.java
@@ -4,22 +4,17 @@ public class Problems300 {
     public int lengthOfLIS(int[] nums) {
         if(nums.length == 0) return 0;
         int n = nums.length;
-
         int[] dp = new int[n];
 
-        for(int i = 0; i < n; i++)
-            dp[i] = 1;
-
-        int top = nums[0];
+        dp[0] = 1;
         int max = 1;
 
-        // Initial commit without checking answers
         for(int i = 1; i < n; i++) {
-            if(nums[i] > nums[i - 1]) {
-                top = nums[i];
-                dp[i] = dp[i - 1] + 1;
-            } else {
-                dp[i] = dp[i - 1];
+            dp[i] = 1;
+            for(int j = 0; j < i; j++) {
+                if (nums[i] > nums[j] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
             }
             if(max < dp[i])
                 max = dp[i];
